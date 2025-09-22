bac94eb1b9af3a17810840325519557119fd4b2e
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-09-22 10:26:15 +0300

    intialize services

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import randomBytes from "crypto";
+let posts = {};
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("from home");
+});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  const post = Object.values(posts);
+  return res.status(201).json({
+    post,
+  });
+});
+app.get("/posts:id", (req, res) => {
+  const id = req.params;
+});
+app.listen(3000, () => {
+  console.log("Server start:PORT:3000");
+});
