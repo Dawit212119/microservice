0dc5da060f92958c6ddeec10c0d41871a922d4c3
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-09-25 15:29:31 +0300

    eslint and prettier config for query and eventBus services

--- query/index.js
+++ query/index.js
@@ -1,63 +1,66 @@
-import express from "express";
-import axios from "axios";
+import express from 'express';
+import axios from 'axios';
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
 const posts = {};
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   console.log(req.body);
-  if (req.body.type === "PostCreated") {
+  if (req.body.type === 'PostCreated') {
     const { type, data } = req.body;
+    console.log(type);
+
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
     const post = posts[id];
     console.log(post);
     res.status(201).json({
-      message: "post created",
+      message: 'post created',
       post,
     });
   }
-  if (req.body.type === "CommentCreated") {
+  if (req.body.type === 'CommentCreated') {
     const { type, data } = req.body;
+    console.log(type);
     const { PostId, id, message } = data;
     const post = posts[PostId];
     post.comments.push({ id, message });
     const commentAdd = posts[PostId];
     res.status(201).json({
-      message: "comment added",
+      message: 'comment added',
       commentAdd,
     });
   }
 });
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
-const handleEvent = (event) => {
+const handleEvent = event => {
   console.log(event);
-  if (event.type === "PostCreated") {
+  if (event.type === 'PostCreated') {
     posts[event.data.id] = {
       id: event.data.id,
       title: event.data.title,
       comments: [],
     };
   }
-  if (event.type === "CommentCreated") {
+  if (event.type === 'CommentCreated') {
     const post = posts[event.PostId];
     post.comments.push({ id: event.id, message: event.message });
   }
 };
 const missedEvent = async () => {
   try {
-    const event = await axios.get("http://localhost:3005/events");
-    for (let eve of event.data) {
+    const event = await axios.get('http://localhost:3005/events');
+    for (const eve of event.data) {
       console.log(eve);
       handleEvent(eve);
     }
   } catch (err) {
-    console.log("coundt sync events", err);
+    console.log('coundt sync events', err);
   }
 };
 app.listen(PORT, async () => {
-  console.log("server start", PORT);
+  console.log('server start', PORT);
   await missedEvent();
 });
