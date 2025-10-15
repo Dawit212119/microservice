b695e69634eaaf964854219de984a644a72d1774
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-10-16 02:32:12 +0300

    setup skaffold

--- tickting/auth/src/index.ts
+++ tickting/auth/src/index.ts
@@ -2,7 +2,10 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
-
+app.get("/api/users/currentuser", (req, res) => {
+  console.log("home");
+  res.send({});
+});
 app
   .listen(3000, () => {
     console.log("auth start at 3000");
