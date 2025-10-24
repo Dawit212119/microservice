3c296e189185e566b068bda18d696a882b30494f
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-10-24 22:34:21 +0300

    implement currentUSER route

--- tickting/auth/src/routes/currentuser.route.ts
+++ tickting/auth/src/routes/currentuser.route.ts
@@ -1,9 +1,24 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send({});
+  if (!req.session?.jwt) {
+    return res.send({
+      currentUser: null,
+    });
+  }
+  console.log(req.session);
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.send({
+      currentUser: payload,
+    });
+  } catch (error) {
+    return res.send({
+      currentUser: null,
+    });
+  }
 });
 
 export { router as CurrentUserRoute };
