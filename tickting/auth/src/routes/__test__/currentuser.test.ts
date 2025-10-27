6672df5c9efc2a1122df8960437ea1f89a0d8ba0
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-10-27 23:57:25 +0300

    complete auth service test

--- tickting/auth/src/routes/__test__/currentuser.test.ts
+++ tickting/auth/src/routes/__test__/currentuser.test.ts
@@ -24,3 +24,27 @@ it("return 400", async () => {
   expect(res.body).toHaveProperty("errors", [{ message: "Not Authorize" }]);
   console.log(res.body);
 });
+
+///////  jest course
+
+it("1+1 return 2", () => {
+  expect(1 + 1).toBe(2); //  toBe is used for boolean,string,number
+});
+
+type ob = {
+  one: number;
+  two?: any;
+};
+it("to equal", () => {
+  const object: ob = { one: 1 };
+  object["two"] = "two";
+  expect(object).toEqual({
+    // toEqual is used for object,array,(non primitive)
+    one: 1,
+    two: "two",
+  });
+});
+
+it("0 is falsy", () => {
+  expect(0).toBeFalsy(); // it will success if the value is either 0,null,undefined,
+});
